21c97f051c80dec627e55b12039455d25bc82a25
Amr Sono <amrsono@gmail.com>
2026-01-21 21:55:12 +0200

    Cleanup debug files

--- test-schema.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from './lib/supabase';
-
-async function test() {
-    const { data, error } = await supabase.from('orders').select('*').limit(1);
-    console.log(JSON.stringify(data, null, 2));
-}
-
-test();
